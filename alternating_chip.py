78a59606d6aac351fb8aef2d87d331ec205b18df
bonczol <filip.bonczyk@student.put.poznan.pl>
2019-05-25 00:18:48 +0200

    run script with arguments

--- alternating_chip.py
+++ alternating_chip.py
@@ -4,7 +4,8 @@ from DataLoader import DataLoader
 
 
 def main():
-	URL = 'http://piotr.e.wawrzyniak.doctorate.put.poznan.pl/bio.php?n=500&k=10&mode=alternating&intensity=0&position=0&sqpe=100&sqne=0&pose=0'
+	URL = 'http://piotr.e.wawrzyniak.doctorate.put.poznan.pl/bio.php?n='+ sys.argv[1] + '&k=' + sys.argv[2] + '&mode=alternating&intensity=0&position=0&sqpe=' + sys.argv[3] + '&sqne=0&pose=0'
+	print("URL:", URL)
 	loader = DataLoader(URL)
 	data = loader.getData()
 	sequencer = Sequencer(data["set1"], data["set2"], data["len_DNA"], data["len_oligo"], data["start_seq"])
