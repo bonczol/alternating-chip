306afd8db251cc7632a6352e9e1b53cf1667dbcf
bonczol <filip.bonczyk@student.put.poznan.pl>
2019-05-24 22:57:13 +0200

    almost working

--- DataLoader.py
+++ DataLoader.py
@@ -7,8 +7,10 @@ class DataLoader:
 	def __init__(self, URL):
 		self.URL = URL;
 
+
 	def sendRequest(self):
 		 return requests.get(self.URL)
+	
 	
 	def getData(self):
 		response = self.sendRequest()
@@ -16,10 +18,10 @@ class DataLoader:
 
 		data = dict()
 		data["len_DNA"] = int(root.attrib["length"])
-		data["start_seq"] = root.attrib["start"] + "A"
-		data["len_oligo"] = int(len(data["start_seq"])/ 2)
+		data["start_seq"] = root.attrib["start"]
+		data["len_oligo"] = int((len(data["start_seq"]) + 1)/ 2)
 		data["set1"] = [ Oligo(child.text) for child in root[0]]
-		data["set2"] = [ Oligo(child.text) for child in root[1]]
+		data["set2"] = [ child.text for child in root[1]]
 
 		return data
 
--- alternating_chip.py
+++ alternating_chip.py
@@ -7,10 +7,8 @@ def main():
 	URL = 'http://piotr.e.wawrzyniak.doctorate.put.poznan.pl/bio.php?n=500&k=10&mode=alternating&intensity=0&position=0&sqpe=100&sqne=0&pose=0'
 	loader = DataLoader(URL)
 	data = loader.getData()
-	print("set length:", len(data["set1"]))
 	sequencer = Sequencer(data["set1"], data["set2"], data["len_DNA"], data["len_oligo"], data["start_seq"])
-
-	#print(sequencer.run())
+	print("Result:", sequencer.run())
 
 
 if __name__ == '__main__':
